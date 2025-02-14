d5632af30244e9da2b22ffe930ffe173ee6fc6c9
James Bremner <j.bremner@ymail.com>
2025-02-14 11:50:54 +0000

    remove volume control

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
 
 const video = document.querySelector("video");
-const volumeInput = document.querySelector("#volume");
+//const volumeInput = document.querySelector("#volume");
 const captions = document.querySelectorAll("span");
+const captionContainer = document.querySelector("p");
+
+console.log(captionContainer);
+
+//function to convert data
+// if the timestamp start time contains "1:"
+// get the start time and add 60 seconds
+const addSixtySec = (startTime) => {
+   
+}
+
+//add functionality to skip to proper time in video when a span is clicked
+//add event listener to spans
+//get the start time of span that has been clicked
+//move the video to that start time 
+
+captionContainer.addEventListener("click", (event) => {
+    const spanStartTime = event.target.dataset.start;
+    //console.log(spanStartTime);
+    //let newTime = spanStartTime;
+
+    if (spanStartTime.includes("1:")) {
+        spanStartTime.replace("1:", "")
+        //convert spanStartTime to number
+        console.log(spanStartTime);
+        let newTime = parseInt(spanStartTime );
+        console.log(newTime);
+
+    }
+    //set video current time to clicked span start time
+    try {
+        video.currentTime = event.target.dataset.start;
+    } catch (error) {
+        console.log(error);
+    }
+ 
+})
 
 //updates the volume of the video
 const setVolume = (volume) => {
@@ -24,14 +61,15 @@ const colorCaption = (timestamp) => {
 
 //add event listener to video to check when the time is changing
 video.addEventListener("timeupdate", (event) => {
+    console.log(video.currentTime);
     colorCaption(video.currentTime);
 
 })
 
 //add event listener to volume slider to update volume of video
-volumeInput.addEventListener("change", (event) => {
-    setVolume(event.target.value / 10);
-})
+// volumeInput.addEventListener("change", (event) => {
+//     setVolume(event.target.value / 10);
+// })
 
 
 
